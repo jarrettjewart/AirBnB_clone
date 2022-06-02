c29070b6d776891261627bebe33e9b9234c6dcff
AnActualBanana <loganmcclain59@gmail.com>
2022-06-02 10:25:33 -0700

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -12,29 +12,24 @@ class BaseModel:
 other classes"""
     def __init__(self, *args, **kwargs):
         """instantiation with id, and datetime for creation and update"""
-        if kwargs is not None:
+        if kwargs:
             for (key, value) in kwargs.items():
                 if key == "__class__":
                     pass
                 if key == "created_at":
                     self.created_at = datetime.fromisoformat(value)
-                else:
-                    self.created_at = datetime.now()
                 if key == "updated_at":
                     self.updated_at = datetime.fromisoformat(value)
-                else:
-                    self.updated_at = datetime.now()
                 if key == "id":
-                    self.id = value
-                else:
-                    self.id = uuid4()
+                    self.id = str(value)
         else:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.id = uuid4()
+            self.id = str(uuid.uuid4())
     def __str__(self):
         """returns class name, id, and dictionary"""
-        return("[{}] ({}) {}".format(type(self), self.id, self.__dict__))
+        return("[{}] ({}) {}".format(str(self.__class__.__name__),\
+               str(self.id), self.__dict__))
 
     def save(self):
         """Updates the updated_at attribute with the current datetime"""
