c64c3716f03038f28bf2da2be69ea2619b813d8c
jarrettjewart <92384372+jarrettjewart@users.noreply.github.com>
2022-06-06 18:12:08 -0500

    Update file_storage.py
    Changed comments  for easier read

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,7 +17,9 @@ classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
 
 
 class FileStorage:
-    """serializes instances to a JSON file & deserializes back to instances"""
+    """
+    serializes instances to a JSON file & deserializes back to instances
+    """
 
     # string - path to the JSON file
     __file_path = "file.json"
@@ -25,17 +27,23 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        """returns the dictionary __objects"""
+        """
+        returns the dictionary __objects
+        """
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
         json_objects = {}
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
@@ -43,7 +51,9 @@ class FileStorage:
             json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """
+        deserializes the JSON file to __objects
+        """
         try:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
